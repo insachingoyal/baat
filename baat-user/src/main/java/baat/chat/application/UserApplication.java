82b68ed1a72a64fea44f922da8f24d40b62c5e34
Sachin Goyal <sachin@fanduel.com>
2017-10-19 23:30:58 +0100

    added baat-user

---
+++ baat-user/src/main/java/baat/chat/application/UserApplication.java
@@ -0,0 +1,13 @@
+package baat.chat.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("baat")
+public class UserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
