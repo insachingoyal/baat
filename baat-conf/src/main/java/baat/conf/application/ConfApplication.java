a1c379a1f042bea80467dd015699d258876772af
Sachin Goyal <sachin@fanduel.com>
2017-10-20 22:40:08 +0100

    added config

--- baat-conf/src/main/java/baat/conf/application/ConfApplication.java
+++ baat-conf/src/main/java/baat/conf/application/ConfApplication.java
@@ -1,4 +1,4 @@
-package baat.chat.application;
+package baat.conf.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("baat")
-public class UserApplication {
+public class ConfApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(ConfApplication.class, args);
 	}
 }
--- baat-user/src/main/java/baat/user/controller/UserController.java
+++ baat-user/src/main/java/baat/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package baat.chat.controller;
+package baat.user.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
