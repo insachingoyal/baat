242b9d0c5a68a872e25ea416a0aac912f368ffb7
Sachin Goyal <sachin@fanduel.com>
2017-10-29 23:45:08 +0000

    added basic security

---
+++ baat-common/src/main/java/baat/common/constants/Constants.java
@@ -0,0 +1,5 @@
+package baat.common.constants;
+
+public class Constants {
+	public static final String X_AUTH_TOKEN = "X-Auth-Token";
+}
--- baat-web/src/main/java/baat/web/controller/WebController.java
+++ baat-web/src/main/java/baat/web/controller/WebController.java
@@ -1,13 +1,23 @@
 package baat.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static baat.common.constants.Constants.X_AUTH_TOKEN;
 
 @Controller
 public class WebController {
-
 	@RequestMapping(value = "/")
-	public String index() {
-		return "index.html";
+	public String index(@RequestHeader(value = X_AUTH_TOKEN) final String userToken) {
+		if (validUserToken(userToken)) {
+			return "home/home.html";
+		} else {
+			return "login/login.html";
+		}
+	}
+
+	private boolean validUserToken(final String userToken) {
+		return false;
 	}
 }
