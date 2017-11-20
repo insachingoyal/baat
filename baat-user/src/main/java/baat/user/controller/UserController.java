79bb85939baa73363383e38b83fc5b5d56f0ead1
Sachin Goyal <sachin@fanduel.com>
2017-11-20 21:15:20 +0000

    CORS

--- baat-user/src/main/java/baat/user/controller/UserController.java
+++ baat-user/src/main/java/baat/user/controller/UserController.java
@@ -9,20 +9,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
 @RestController
 public class UserController {
 
 	@Autowired
 	UserService userService;
 
-	@CrossOrigin(origins = "*")
-	@RequestMapping("/authenticate")
+	@CrossOrigin
+	@RequestMapping(value = "/authenticate", method = PUT)
 	public String authenticate(@RequestBody final UserCredentials credentials) {
 		return userService.authenticate(credentials);
 	}
 
-	@CrossOrigin(origins = "*")
-	@RequestMapping("/signup")
+	@CrossOrigin
+	@RequestMapping(value = "/signup", method = PUT)
 	public String signup(@RequestBody final SignupRequest signupRequest) {
 		return userService.signup(signupRequest);
 	}
