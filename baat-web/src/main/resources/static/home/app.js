225629e5f6974fe445cffcf52ce3a3002d44eaf1
Sachin Goyal <sachin@fanduel.com>
2017-12-05 22:57:59 +0000

    basic chatting working

--- baat-web/src/main/resources/static/home/app.js
+++ baat-web/src/main/resources/static/home/app.js
@@ -1,30 +1,42 @@
 $(document).ready(function () {
 
+    var userToken = Cookies.get("X-Auth-Token");
+
     $.ajax({
         contentType: 'application/json',
         dataType: 'json',
         success: function (userInfo) {
-            updateUserDetails(userInfo);
-            createWebSocket(userInfo.id);
+            showCurrentUserInfo(userInfo);
+            createWebSocket(userToken);
             populateUserList();
+            bindSendButton(userToken, userInfo);
         },
         error: function (error) {
-            $('#error-message').text(error)
+            $('#error-message').text(error.responseText)
         },
         processData: false,
         type: 'GET',
-        url: "http://" + location.hostname + ":8081/userForToken/" + Cookies.get("X-Auth-Token")
+        url: "http://" + location.hostname + ":8081/userForToken/" + userToken
     });
 
-    function updateUserDetails(userInfo) {
+    function showCurrentUserInfo(userInfo) {
         $('#user-details').text("Hi " + userInfo.fullName + "!");
     }
 
-    function createWebSocket(userId) {
+    function createWebSocket(userToken) {
         var ws = new WebSocket("ws://" + location.hostname + ":8083/baat-ws");
 
         ws.onmessage = function (event) {
-            $("#messages").append("<p>" + event.data + "</p>");
+            var replyMessage = JSON.parse(event.data),
+                $userLink,
+                senderUserFullName;
+            if (replyMessage.senderUserId && replyMessage.textMessage) {
+                $userLink = $("#user-list").find("a[data-user-id=" + replyMessage.senderUserId + "]");
+                senderUserFullName = $userLink.text();
+
+                $userLink.addClass('btn-info');
+                $("#message-history-panel").append("<p>" + senderUserFullName + ": " + replyMessage.textMessage + "</p>");
+            }
         };
 
         ws.onclose = function () {
@@ -32,7 +44,8 @@ $(document).ready(function () {
         };
 
         ws.onopen = function () {
-            ws.send(userId);
+            // TODO may want to do it as part of Connect
+            ws.send(userToken);
             console.log("Connected");
         };
     }
@@ -45,14 +58,33 @@ $(document).ready(function () {
                 for (var i = 0; i < userInfos.length; i++) {
                     var userInfo = userInfos[i];
                     $("#user-list").append("<li>" +
-                        "<a id='user-" + userInfo.id + "' data-user-id='" + userInfo.id + "' href='#' >" +
+                        "<a id='user-" + userInfo.id + "' data-user-id='" + userInfo.id + "' href='#' class='user-link btn btn-default''>" +
                         "<strong>" + userInfo.fullName + "</strong>" +
                         "</a>" +
-                        "</li>")
+                        "</li>");
                 }
+
+                $("body").on("click", "a.user-link", function () {
+                    var $userLink = $(this),
+                        $allUserLinks = $("a.user-link"),
+                        userId = $userLink.data("user-id");
+
+
+                    $allUserLinks.removeClass("btn-primary");
+                    $allUserLinks.addClass("btn-default");
+
+                    $userLink.removeClass("btn-default");
+                    $userLink.removeClass("btn-info");
+                    $userLink.addClass("btn-primary");
+
+                    // TODO load user chat history
+                    $("#message-history-panel").empty();
+
+                    $("#recipient-user-id").val(userId);
+                });
             },
             error: function (error) {
-                $('#error-message').text(error)
+                $('#error-message').text(error.responseText)
             },
             processData: false,
             type: 'GET',
@@ -60,27 +92,30 @@ $(document).ready(function () {
         });
     }
 
-    $('#send-button').click(function () {
-        var userToken = $("#from-user").val();
-        var recipientUserId = $("#to-user").val();
-        var textMessage = $("#new-message").val();
-        var chatMessage = JSON.stringify({
-            "senderUserToken": userToken,
-            "recipientUserId": recipientUserId,
-            "textMessage": textMessage
-        });
+    function bindSendButton(userToken, userInfo) {
+        $('#send-button').click(function () {
+            var recipientUserId = $("#recipient-user-id").val(),
+                textMessage = $("#new-message").val(),
+                chatMessage = JSON.stringify({
+                    "senderUserToken": userToken,
+                    "recipientUserId": recipientUserId,
+                    "textMessage": textMessage
+                });
 
-        $.ajax({
-            contentType: 'application/json',
-            data: chatMessage,
-            dataType: 'json',
-            success: function (data) {
-            },
-            error: function () {
-            },
-            processData: false,
-            type: 'PUT',
-            url: "http://" + location.hostname + ":8084/"
+            $("#message-history-panel").append("<p>" + userInfo.fullName + ": " + textMessage + "</p>");
+            $("#new-message").val("");
+
+            $.ajax({
+                contentType: 'application/json',
+                data: chatMessage,
+                dataType: 'json',
+                error: function (error) {
+                    $('#error-message').text(error.responseText)
+                },
+                processData: false,
+                type: 'PUT',
+                url: "http://" + location.hostname + ":8084/"
+            });
         });
-    });
+    }
 });
